3cbd97f2e70ceca2feeb68bdf0e85cad488d31f6
1natsu172 <1natsu172@users.noreply.github.com>
2018-12-06 14:33:24 +0900

    :art: feat: implement converters
    feat pxTo...

---
+++ src/converters/converters.ts
@@ -0,0 +1,23 @@
+import { Unit } from '../types'
+
+const isPx = (value: string): boolean => String(value).includes('px')
+
+const pxToRelativeUnit = (value: string | number, ratio = 16, unit: Unit) => {
+  let val = value
+
+  if (typeof val === 'string') {
+    if (!isPx(val)) throw new Error('An unexpected unit was handed')
+
+    val = parseInt(val, 10)
+  }
+
+  val = val / ratio
+
+  return val + unit
+}
+
+export const pxToEm = (value: string | number, ratio = 16) =>
+  pxToRelativeUnit(value, ratio, 'em')
+
+export const pxToRem = (value: string | number, ratio = 16) =>
+  pxToRelativeUnit(value, ratio, 'rem')
